d31f76062f3a24eb7bc50f0eb62fb4d1409c6377
Connor Patterson <wateryflapjackk@gmail.com>
2025-08-02 21:02:37 -0500

    fix(*): resume

--- src/app/pages/resume/resume.ts
+++ src/app/pages/resume/resume.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import resumeData from './resume.json'
+import { DOCUMENT } from '@angular/common'
+import { Inject } from '@angular/core'
 
 @Component({
   selector: 'app-resume',
@@ -9,5 +11,14 @@ import resumeData from './resume.json'
   styleUrl: './resume.scss'
 })
 export class ResumeComponent {
+  baseHref: string;
   resume = resumeData.resume
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.baseHref = this.document.getElementsByTagName('base')[0].href;
+  }
+
+  getResumeDownloadLink(): string {
+    return this.baseHref + 'assets/images/ConnorPattersonResume2025.pdf';
+  }
 }
